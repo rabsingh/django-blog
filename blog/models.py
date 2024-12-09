456bca60fd2d8d8a452c10a078e79945e2484ac3
Rabinder Singh <rab_singh@hotmail.com>
2024-12-09 16:25:29 +0000

    Corrected class Comment

--- blog/models.py
+++ blog/models.py
@@ -26,7 +26,7 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    class Comment(models.Model):
+    
         post = models.ForeignKey(
             Post, on_delete=models.CASCADE, related_name="comments")
         author = models.ForeignKey(
