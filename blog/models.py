a6afe1c74bc0f515bee87828d530a1d0a0b59837
Rabinder Singh <rab_singh@hotmail.com>
2024-12-09 16:30:58 +0000

    Corrected class Comment again for indentation

--- blog/models.py
+++ blog/models.py
@@ -26,17 +26,16 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    
-        post = models.ForeignKey(
-            Post, on_delete=models.CASCADE, related_name="comments")
-        author = models.ForeignKey(
-            User, on_delete=models.CASCADE, related_name="commenter")
-        body = models.TextField()
-        approved = models.BooleanField(default=False)
-        created_on = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
         
-        class Meta:
+    class Meta:
             ordering = ["created_on"]
     # …
-        def __str__(self):
+    def __str__(self):
             return f"Comment {self.title} by {self.author}"
